dafc948baa2d648cdcec24eb50253628e2151170
dicook <visnut@gmail.com>
2023-08-12 08:11:06 +1000

    added the example in but still needs re-working of text

--- woylier-article.R
+++ woylier-article.R
@@ -168,18 +168,23 @@ kbl(tbl) %>%
 #> tourr::animate_xy(<data>, tour_path = woylier::grand_tour_givens())
 
 
-## ----echo=TRUE----------------------------------------------------------------
-set.seed(2022)
-p <- 6
-base1 <- tourr::basis_random(p, d=2)
-base2 <- tourr::basis_random(p, d=2)
+## ----echo=TRUE, eval=FALSE----------------------------------------------------
+#> set.seed(2022)
+#> p <- 6
+#> base1 <- tourr::basis_random(p, d=2)
+#> base2 <- tourr::basis_random(p, d=2)
+#> 
+#> base1
+#> base2
 
-base1
-base2
+
+## ----echo=TRUE, eval=FALSE----------------------------------------------------
+#> givens_full_path(base1, base2, nsteps = 5)
 
 
-## ----echo=TRUE----------------------------------------------------------------
-givens_full_path(base1, base2, nsteps = 5)
+## ----compare-paths, echo=FALSE, eval=TRUE, out.width="80%", fig.width=6, fig.height=10, fig.align="center", fig.cap="Comparison of Givens path and geodesic path between 2D projections. The Givens path preserves the frame ending at the provided basis (frame), while geodesic is agnostic to the particular basis. In general the geodesic is preferred because it removes within-plane spin, but occasionally it is helpful to very specifically arrive at the prescribed basis."----
+# from code in comparison_plot_table.R
+knitr::include_graphics("compare-paths.png")
 
 
 ## ----echo = FALSE, eval=FALSE-------------------------------------------------
@@ -250,11 +255,11 @@ givens_full_path(base1, base2, nsteps = 5)
 #>                   "sphere.gif")
 
 
-## ----1d-path-dynamic, out.width="45%", fig.align="center", echo = FALSE, fig.height = 3, fig.show='hold', fig.cap="Interpolation steps of 1D and 2D projections of 3D data", include=knitr::is_html_output(), eval=knitr::is_html_output(), fig.alt = "Two highlighted points on the surface of sphere connected by 10 interpolated steps, rotating."----
+## ----1d-path-dynamic, out.width="45%", fig.align="center", echo = FALSE, fig.height = 3, fig.show='hold', fig.cap="Interpolation steps of 1D (left) and 2D (right) projections of 3D data made with a Givens path (forest green) and a geodesic path (deep red). The cream points represent the space of all projections, which is a sphere for 1D projections and a torus for 2D projections. In the 1D example, geodesic arrives at the opposite side of the sphere to Givens, indicating that it has flipped the direction of the vector in order to make the shortest path to the same plane. A similar thing happens for the 2D example, geosesic flips the sign of one basis vector, but it defines the same plane, as indicated by the cream circles.", include=knitr::is_html_output(), eval=knitr::is_html_output(), fig.alt = "Two highlighted points on the surface of sphere connected by 10 interpolated steps, rotating."----
 #> knitr::include_graphics(c("sphere.gif", "torus.gif"))
 
 
-## ----1d-path-static, out.width="50%", fig.align="center", echo = FALSE, fig.height = 3, fig.show='hold', fig.cap="Interpolation steps of 1D projections of 6D data", include=knitr::is_latex_output(), eval=knitr::is_latex_output(), fig.alt = "2 highlighted points on the surface of sphere connected by 10 interpolated steps, rotating."----
+## ----1d-path-static, out.width="50%", fig.align="center", echo = FALSE, fig.height = 3, fig.show='hold', fig.cap="Interpolation steps of 1D projections of 6D data", include=knitr::is_latex_output(), eval=knitr::is_latex_output(), fig.alt = "Two highlighted points on the surface of sphere connected by 10 interpolated steps, rotating."----
 knitr::include_graphics(c("sphere_static.png", "torus_static.png"))
 
 
